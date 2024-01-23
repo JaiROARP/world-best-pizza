868ccf7026f91cd06385e815abff4b1eaf4fad1d
Jairo A. Robledo <jairoarpf91@gmail.com>
2024-01-23 20:49:42 -0300

    starting js

---
+++ assets/scripts/script.js
@@ -0,0 +1,22 @@
+
+
+//DOM
+//querySelector = Devuelve un elemento del DOM que coincida con el selector dado
+//querySelectorAll = Devuelve todos los elementos del DOM que coincidan con el selector dado
+
+/*
+let links = document.querySelectorAll("a");
+
+links.forEach(function (link) {    
+    console.log(link);
+})
+*/
+
+let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function (td) {
+   td.addEventListener('click', function(){
+
+   })
+})
+
