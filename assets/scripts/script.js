017aa8968f6119b250ee3302d1c9b11044ccefaa
Jairo A. Robledo <jairoarpf91@gmail.com>
2024-01-25 19:34:08 -0300

    finish

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,6 +4,7 @@
 //querySelector = Devuelve un elemento del DOM que coincida con el selector dado
 //querySelectorAll = Devuelve todos los elementos del DOM que coincidan con el selector dado
 
+// -----------------------QUERYSELECTOR----------------------
 /*
 let links = document.querySelectorAll("a");
 
@@ -12,6 +13,7 @@ links.forEach(function (link) {
 })
 */
 
+// -------------------EVITAR EL EVENTO POR DEFECTO----------------
 /*
 // De esta forma evitamos el evento por defecto
 let closes = document.querySelectorAll(".close");
@@ -26,7 +28,8 @@ closes.forEach(function (close) {
     });
 });
 */
-
+// -------------------REMOVER CLASES-------------------------
+/*
 // De esta forma removemos clases
 let iconos = querySelectorAll("i");
 
@@ -34,4 +37,40 @@ iconos.forEach(function (icono) {
     
     icono.classList.remove("fa-star");
 })
+*/
+
+
+// --------------- AñADIR CLASES--------------------
+
+let links = document.querySelectorAll(".close");
+links.forEach(function (link) {
+
+    //Añadimos un evento click a los links
+    link.addEventListener('click', function (ev) {
+        ev.preventDefault();
+        let content = document.querySelector(".content");
+
+        //Quitamos las clases de animacion: animate__animated y animate__zoomInDown
+        content.classList.remove("animate__animated");
+        content.classList.remove("animate__zoomInDown");
+        
+        //Añadimos las clases de animacion: animate__animated y animate__bounceOutUp
+        content.classList.add("animate__bounceOutUp");
+        content.classList.add("animate__animated");
+
+        //Despues de 600ms volvemos a la pagina principal
+        setTimeout(function () {
+            location.href = "/";
+        }, 600);        
+     
+    });
+});
+
+
+
+
+
+
+
+
 
